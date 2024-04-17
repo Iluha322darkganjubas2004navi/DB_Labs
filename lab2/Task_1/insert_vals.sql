88102a9866605103e6f788315d2e071e490212c8
Sanya_Pudge <90987903+Iluha322darkganjubas2004navi@users.noreply.github.com>
2024-04-17 15:00:11 +0300

    Update insert_vals.sql

--- lab2/Task_1/insert_vals.sql
+++ lab2/Task_1/insert_vals.sql
@@ -1,6 +1,10 @@
 insert into groups(id,name,c_val) values (null,'153504',0)
 
+DELETE FROM groups WHERE id = 1;
+delete_students_by_name('Имя_студента_для_удаления');
+ALTER TRIGGER trigger_update_c_val ENABLE;
 
+  
 insert into students (id,name,group_id) values(null, 'Silniy chel',1)
 
 DECLARE
